fe73800ad7016ac925692eab381c33d607433be7
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2025-06-11 12:16:36 +0200

    No longer need to deduplicate records
    This is apparently now done server-side. This broke in October 2024 without me noticing. Whoops!

--- fetch_all.py
+++ fetch_all.py
@@ -92,13 +92,8 @@ def fetch_record_attachment(record):
 
 
 def fetch_newest_records(records):
-    by_pubkeyhash = dict()
-    for record in records:
-        # This intentionally overwrites old records if a newer record for the same pubkey exists.
-        by_pubkeyhash[record["pubKeyHash"]] = record
-    print(f"Found {len(by_pubkeyhash)} unique pubkeys in {len(records)} records.")
     pems = []
-    for record in by_pubkeyhash.values():
+    for record in records[:10]:
         pems.append(fetch_record_attachment(record))
     return pems
 
